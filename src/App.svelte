90f9d9c2fe2ad2add808f77372224dd3b0104d6b
FedericoVarela <federicovr02@gmail.com>
2020-09-07 20:24:13 -0300

    Added starter template

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { Router } from "@sveltech/routify";
+  import { routes } from "../.routify/routes";
+
+  let hola : string = "Hola"
+  enum Si {
+    COSO,
+    AAAAAAAAA,
+    SI,
+    V_O_I_D
+  }
+
+  const boludeces : Array<Si> = [
+    Si.COSO, Si.AAAAAAAAA, Si.AAAAAAAAA, Si.V_O_I_D
+  ]
+</script>
+
+<style  global>
+  @import "../static/global.css";
+</style>
+
+<h1>{hola}</h1>
+<Router {routes} />
+
+{#each boludeces as b }
+  {b}
+{/each}
