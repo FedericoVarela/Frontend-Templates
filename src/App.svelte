976a5fc6405edf213b9b9cb31ff71f24220d0791
FedericoVarela <federicovr02@gmail.com>
2020-09-13 12:55:17 -0300

    Added Typescript exclusive code to check if it works

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	export let name: string;
+	
+	type TODO = [string, boolean]
+	const todos : Array<TODO> = [
+		["Clone the template", true],
+		["Run the server", true],
+		["Finish app", false],
+	]
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<ul>
+		{#each todos as t }
+			<li>{t[0]}: {t[1] ? "✓" : ""}</li>
+		{/each}
+	</ul>
 </main>
 
 <style>
